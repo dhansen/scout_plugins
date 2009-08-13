0eb20165ac4a9b9c447351ee0e39d1d00dc40fad
Dan Hansen <dhansen@triand.com>
2009-08-13 01:23:03 -0500

    mysql expanded connection monitoring

--- mysql.rb
+++ mysql.rb
@@ -3,9 +3,10 @@
 #
 
 require 'set'
-
+ 
 class MysqlQueryStatisticsTriand < Scout::Plugin
-  ENTRIES = %w(Com_insert Com_select Com_update Com_delete Connections Max_used_connections).to_set
+  ENTRIES = %w(Com_insert Com_select Com_update Com_delete).to_set
+  # ENTRIES_SINGLE = %w(Connections Max_used_connections).to_set
   
   needs "mysql"
 
@@ -17,20 +18,22 @@ class MysqlQueryStatisticsTriand < Scout::Plugin
     port     = get_option(:port)
     socket   = get_option(:socket)
     
+    
     now = Time.now
     mysql = Mysql.connect(host, user, password, nil, (port.nil? ? nil : port.to_i), socket)
     result = mysql.query('SHOW /*!50002 GLOBAL */ STATUS')
-
+    
+    report_hash = {}
     rows = []
     total = 0
     result.each do |row| 
       rows << row if ENTRIES.include?(row.first)
-
       total += row.last.to_i if row.first[0..3] == 'Com_'
+    
+      report_hash[row.first] = row.last.to_i if ENTRIES_SINGLE.include?(row.first)
     end
     result.free
-
-    report_hash = {}
+    
     rows.each do |row|
       name = row.first[/_(.*)$/, 1]
       value = calculate_counter(now, name, row.last.to_i)
@@ -38,7 +41,9 @@ class MysqlQueryStatisticsTriand < Scout::Plugin
       next unless value
       report(name => value)
     end
-
+    
+    report_hash.each_pair{|name, value| report(name => value) }
+    
 
     total_val = calculate_counter(now, 'total', total)
     report('total' => total_val) if total_val
