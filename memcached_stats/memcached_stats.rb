bc648d6e88c04aad2acaf0f6afe181498861208f
Dan Hansen <dhansen@triand.com>
2010-01-07 23:16:58 -0600

    fixes for memcached monitoring

--- memcached_stats/memcached_stats.rb
+++ memcached_stats/memcached_stats.rb
@@ -28,38 +28,14 @@ class MemcachedMonitor < Scout::Plugin
 
   attr_accessor :connection  
   
-  OPTIONS = <<-HEREDOC
-options:
-  host:
-    name: Host
-    notes: The host to monitor
-    default: 127.0.0.1
-  port:
-    name: Port
-    notes: The port memcached is running on
-    default: 11211
-  key:
-    name: Key to use for testing
-    notes: This key will be both read and set 
-    default: memcached_monitor_key
-  timeout: 
-    name: Timeout
-    notes: maximum amount of time to wait for a response
-    default: 0.25
-  units:
-    name: sizes units for stats fields
-    notes: use either B, KB, MB, GB. Apply only to bytes, limit_maxbytes, bytes_read, bytes_written metrics
-    default: MB
-  metrics:
-    name: stats metrics to monitor
-    notes: see available metrics in http://code.sixapart.com/svn/memcached/trunk/server/doc/protocol.txt
-    default: cmd_get:get_count, cmd_set:set_count, get_misses, get_hits, curr_connections:current_connections, curr_items:total_items, bytes:current_data, limit_maxbytes:max_data
-  rates:
-    name: computed rates stats
-    notes: these values will be computed to expose per/second statistics
-    default: gets_per_sec, sets_per_sec, misses_per_sec, hits_per_sec  
-HEREDOC
-   
+  OPTIONS_HOST        = '127.0.0.1'                   # The host to monitor
+  OPTIONS_PORT        = '11211'                       # The port memcached is running on
+  OPTIONS_KEY         = 'memcached_monitor_key'       # This key will be both read and set 
+  OPTIONS_TIMEOUT     = '0.25'                        # maximum amount of time to wait for a response
+  OPTIONS_UNITS       = 'MB'                          # use either B, KB, MB, GB. Apply only to bytes, limit_maxbytes, bytes_read, bytes_written metrics
+  OPTIONS_METRICS     = 'cmd_get:get_count, cmd_set:set_count, get_misses, get_hits, curr_connections:current_connections, curr_items:total_items, bytes:current_data, limit_maxbytes:max_data'
+  OPTIONS_RATES       = 'gets_per_sec, sets_per_sec, misses_per_sec, hits_per_sec'
+  
   
   def setup_memcache
     begin
@@ -72,8 +48,8 @@ HEREDOC
         raise MissingLibrary, "could not load the memcache gem"
       end
     end
-    # puts "[#{option(:host)}:#{option(:port)}]  key[#{option(:key)}]"
-    self.connection = MemCache.new("#{option(:host)}:#{option(:port)}")
+    # puts "[#{OPTIONS_HOST}:#{OPTIONS_PORT}]  key[#{OPTIONS_KEY}]"
+    self.connection = MemCache.new("#{OPTIONS_HOST}:#{OPTIONS_PORT}")
   end
   
   def build_report(h={})
@@ -87,9 +63,9 @@ HEREDOC
       # if the gem is not loaded, the exception class will not be defined either.
       error("missing library", e.message)
     rescue Timeout::Error => e
-      alert("memcached timeout", "memcached on #{option(:host)}:#{option(:port)} failed to respond within #{timeout_value} seconds")
+      alert("memcached timeout", "memcached on #{OPTIONS_HOST}:#{OPTIONS_PORT} failed to respond within #{timeout_value} seconds")
     rescue MemCache::MemCacheError => e
-      alert("memcache connection failed", "unable to connect to memcached on #{option(:host)}:#{option(:port)}")
+      alert("memcache connection failed", "unable to connect to memcached on #{OPTIONS_HOST}:#{OPTIONS_PORT}")
     rescue TestFailed => e
       alert(e.message)
     end
@@ -98,16 +74,16 @@ HEREDOC
   def test_setting_value
     @test_value = (1..4).collect { |a| VALUE_CHARS[rand(VALUE_CHARS.size)] }.join
     timeout(timeout_value) do
-      connection.set(option(:key), @test_value)
+      connection.set(OPTIONS_KEY, @test_value)
     end
   end
 
   def test_getting_value
     value = timeout(timeout_value) do
-      connection.get(option(:key))
+      connection.get(OPTIONS_KEY)
     end
     if value != @test_value
-      raise TestFailed, "bad data from #{option(:host)}, expected #{@test_value} but got #{value}"
+      raise TestFailed, "bad data from #{OPTIONS_HOST}, expected #{@test_value} but got #{value}"
     end
   end
 
@@ -118,8 +94,8 @@ HEREDOC
     stats = timeout(timeout_value) do
       connection.stats
     end
-    unless (host_stats = stats["#{option(:host)}:#{option(:port)}"])
-      raise(TestFailed, "unable to retrieve stats from #{option(:host)}:#{option(:port)}")
+    unless (host_stats = stats["#{OPTIONS_HOST}:#{OPTIONS_PORT}"])
+      raise(TestFailed, "unable to retrieve stats from #{OPTIONS_HOST}:#{OPTIONS_PORT}")
     end
     
     report_stats = {}
@@ -150,13 +126,13 @@ HEREDOC
   end
   
   def rates_keys
-    return option(:rates).to_s.split(/\s*,\s*/)
+    return OPTIONS_RATES.to_s.split(/\s*,\s*/)
   end
   
   # return a hash to map original metric name as returned by memcached (stats_key) to nicer name as configured in options (report_key)
   def metric_keys_map
     keys_map = {}
-    option(:metrics).to_s.split(/\s*,\s*/).each do |k|
+    OPTIONS_METRICS.to_s.split(/\s*,\s*/).each do |k|
       stats_key, report_key = key_names(k)
       keys_map[stats_key] = report_key
     end
@@ -169,7 +145,7 @@ HEREDOC
   def key_names(k)
     keys = k.split(/\s*:\s*/)
     keys << k if keys.size == 1
-    keys[1] = "#{keys[1]}_#{option(:units)}" if SIZE_METRICS.include?(keys[0])
+    keys[1] = "#{keys[1]}_#{OPTIONS_UNITS}" if SIZE_METRICS.include?(keys[0])
     return keys
   end
     
@@ -191,7 +167,7 @@ HEREDOC
   end
 
   def timeout_value
-    (option(:timeout) || 1).to_f
+    (OPTIONS_TIMEOUT || 1).to_f
   end
 
 end
